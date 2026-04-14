96ea99dc5af3720582c3fac6dfd8c9e2a6984fb8
phuc394 <phuc.thg02394@sinhvien.hoasen.edu.vn>
2026-04-14 15:51:47 +0700

    refactor: Update the Docker file

--- order_service/manage.py
+++ order_service/manage.py
@@ -1,11 +1,8 @@
 # encoding: utf-8
-from flask_script import Manager
 
 from project.app import create_app
 
 app = create_app()
 
-manager = Manager(app)
-
 if __name__ == '__main__':
-    manager.run()
+    app.run(host='0.0.0.0', port=5000)
